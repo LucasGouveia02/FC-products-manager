59e5e2ca2415acce34d4bfb629c33c857e7bf691
Lucas Gouveia <lucaspereiraa81@gmail.com>
2025-04-06 10:21:16 -0300

    Change categoryId to category and use as string in register product

--- src/main/java/com/br/foodconnect/repository/CategoryRepository.java
+++ src/main/java/com/br/foodconnect/repository/CategoryRepository.java
@@ -4,4 +4,6 @@ import com.br.foodconnect.model.CategoryModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<CategoryModel, Long> {
+
+    CategoryModel findByName(String name);
 }

ab4598ebb3a5427923512c6e6d1db439b74b7ada
Lucas Gouveia <lucaspereiraa81@gmail.com>
2025-04-13 20:50:30 -0300

    Fix findAll products to findAllByStoreId now

--- src/main/java/com/br/foodconnect/repository/ProductRepository.java
+++ src/main/java/com/br/foodconnect/repository/ProductRepository.java
@@ -1,7 +1,11 @@
 package com.br.foodconnect.repository;
 
 import com.br.foodconnect.model.ProductModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
+
+    Page<ProductModel> findAllByStoreId(Pageable pageable, Long storeId);
 }

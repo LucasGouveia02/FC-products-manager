fb91fb03c7b8b7843f117068387833465bdbcb03
Lucas Gouveia <lucaspereiraa81@gmail.com>
2025-04-13 21:29:36 -0300

    Find products by name and store id

--- src/main/java/com/br/foodconnect/model/ProductModel.java
+++ src/main/java/com/br/foodconnect/model/ProductModel.java
@@ -9,11 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 import static com.br.foodconnect.util.Utils.arredondarParaDuasCasasDecimais;
 
